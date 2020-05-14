fd4267764669e29c99bc3aa103d824a4ee61f936
Maksym Havrosh <maks.havrosh99@gmail.com>
2020-05-14 14:05:51 +0300

    Add authorization

---
+++ CatInABox/ViewControllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  CatInABox
+//
+//  Created by MG on 14.05.2020.
+//  Copyright © 2020 MG. All rights reserved.
+//
+
+import UIKit
+import SwiftyDropbox
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        DropboxClientsManager.authorizeFromController(UIApplication.shared, controller: self) { (url: URL) -> Void in
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
